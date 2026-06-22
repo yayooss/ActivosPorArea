073a98a0e294d3ed93ec765abfff57c29824314d
yayossyam <y2776430@gmail.com>
2026-06-22 15:51:50 -0600

    first commit

--- config.py
+++ config.py
@@ -2,10 +2,10 @@ from pocketbase import PocketBase
 import os
 
 # --- ASEGURAR QUE LA BD EXISTA EN EL PROYECTO
-BASE_URL = "http://172.21.228.86:8090"
+BASE_URL = "http://TU.RUTA.AQUI"
 pb = PocketBase(BASE_URL)
 
-pb.admins.auth_with_password("practicasryd@palliser.ca", "3**ARAgF")
+pb.admins.auth_with_password("EMAIL@GMAIL.COM", "YOURPASSWORDHERE")
 token = pb.auth_store.token
 
 headers = {"Authorization": f'Bearer {token}'}
